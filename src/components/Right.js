c93a7bdd5b2ecda3cf129ca6d2caa17031e435f0
Aniket <aniketkhangar459@gmail.com>
2024-09-04 23:15:06 +0530

    Done

--- src/components/Right.js
+++ src/components/Right.js
@@ -4,7 +4,7 @@ const Right = ({text}) => {
   return (
    <div style={{margin:"5px 3px", border:"1px solid black", margin:"5px 10px",padding:"20px",}} className='preview'>
         {
-           text.startsWith("#") ? <h1>{text.slice(1)}</h1> : text
+           text.startsWith("#") ? <h1>{text.slice(1).trim()}</h1> : text
         }
    </div>
   )
